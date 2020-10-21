40771afb7bd7c8c3603833bdb73bd632f70d7db5
bomiyu <bom7768@gmail.com>
2020-10-21 11:13:47 +0900

    20201021

--- src/chap10/lecture/throwsKeyword/ThrowsEx1.java
+++ src/chap10/lecture/throwsKeyword/ThrowsEx1.java
@@ -1,5 +1,19 @@
 package chap10.lecture.throwsKeyword;
 
 public class ThrowsEx1 {
+	public static void main(String[] args) {
+		method1();
+	}
 
+	public static void method1() {
+		method2();
+	}
+
+	public static void method2() {
+		method3();
+	}
+
+	public static void method3() {
+		Class.forName("java.lang.String2");
+	}
 }
