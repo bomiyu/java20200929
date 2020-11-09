9543ffe5ba9be9dd0aa1fde41928e058d8fe69cb
bomiyu <bom7768@gmail.com>
2020-11-09 17:15:41 +0900

    2020 11 09

--- src/chap05/textbook/ArrayCopyExample.java
+++ src/chap05/textbook/ArrayCopyExample.java
@@ -5,7 +5,7 @@ public class ArrayCopyExample {
 		String[] oldStrArray = { "java", "array", "copy" };
 		String[] newStrArray = new String[5];
 
-		System.arraycopy(oldStrArray, 0, newStrArray, 0, oldStrArray.length);// oldStrArray.length넣은이유는 3개 다 복사할거니까!
+		System.arraycopy(oldStrArray, 0, newStrArray, 1, oldStrArray.length);// oldStrArray.length넣은이유는 3개 다 복사할거니까!
 
 		for (int i = 0; i < newStrArray.length; i++) {
 			System.out.println(newStrArray[i] + ", ");
